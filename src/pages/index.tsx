23480b22a8cae681673eebdd4dd9df4e7e1d192e
刘蕴皓 <jonhannliu@JonhannLius-MacBook-Pro.local>
2023-03-30 09:54:11 +0800

    feat: 增加注册用户输入提示等功能

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,6 @@ import { useRouter } from "next/router";
 import { useRef, useState } from "react";
 import { CREATE_USER_SUCCESS, FAILURE_PREFIX,CREATE_USER_FAILURE_PERFIX } from "../constants/string";
 import { request } from "../utils/network";
-import { nameValid, passwordValid } from "../utils/valid";
 
 const InitPage = () => {
     const [name, setName] = useState<string>("");
@@ -24,29 +23,6 @@ const InitPage = () => {
             .then((res) => alert(CREATE_USER_SUCCESS))
             .catch((err) => alert(CREATE_USER_FAILURE_PERFIX + err));
     };
-
-    const name_pattern = /^[a-zA-Z0-9_]{3,16}$/;
-    const password_pattern = /^[a-zA-Z0-9_]{6,16}$/
-
-    const checkName = (name_: string) => {
-        setName(name_);
-        if (name_.match(name_pattern) !== null){
-            setNameLegal(true);
-        }
-        else {
-            setNameLegal(false);
-        }
-    };
-
-    const checkPassword = (password_: string) => {
-        setPassword(password_);
-        if (password_.match(password_pattern) !== null) {
-            setPasswordLegal(true);
-        }
-        else {
-            setPasswordLegal(false);
-        }
-    }
 
     return (
         <div style={{padding: 12}}>
