6a679cd30fcc0292b31133f27b7452ef62d16cda
李佳祺 <2021012951@secoder.net>
2023-04-13 21:34:11 +0800

    test: 完善好友关系&info界面测试

--- src/tests/friendrequest.test.tsx
+++ src/tests/friendrequest.test.tsx
@@ -1,42 +1,172 @@
-import { render, screen, waitFor } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import { useRouter } from "next/router";
+import { act } from "react-dom/test-utils";
 import InitPage from "../pages/user/friendrequest";
 
 jest.mock("next/router", () => ({
-  useRouter: jest.fn(),
+    useRouter: jest.fn(),
 }));
 
 const mockRouter = { push: jest.fn() };
-
-describe("InitPage component", () => {
-  beforeEach(() => {
-    (useRouter as jest.Mock).mockReturnValue(mockRouter);
-  });
-
-  it("fetches friend requests and displays them", async () => {
-    const mockRequests = [
-      {
+const mockRequests = [
+    {
         user_id: 1,
         name: "John",
         avatar: "http://example.com/avatar.jpg",
-      },
-      {
+    },
+    {
         user_id: 2,
         name: "Alice",
         avatar: "http://example.com/avatar2.jpg",
-      },
-    ];
-    global.fetch = jest.fn().mockImplementation(() =>
-      Promise.resolve({
-        json: () => Promise.resolve({ code: 0, requests: mockRequests }),
-      })
-    );
+    },
+];
+const mockRequests2 = [
+    {
+        user_id: 1,
+        name: "John",
+        avatar: "http://example.com/avatar.jpg",
+    },
+];
 
-    render(<InitPage />);
+global.fetch = jest.fn();
 
-    await waitFor(() => {
-      expect(screen.getByText(/John/i)).toBeInTheDocument();
-      expect(screen.getByText(/Alice/i)).toBeInTheDocument();
+describe("InitPage component display request list", () => {
+    beforeAll(() => {
+        window.alert = jest.fn();
+        (useRouter as jest.Mock).mockReturnValue(mockRouter);
     });
-  });
+
+    beforeEach(() => {
+        (fetch as jest.Mock).mockImplementation(() =>
+            Promise.resolve({
+                json: () => Promise.resolve({ code: 0, requests: mockRequests }),
+            })
+        );
+    });
+
+    afterEach(() => {
+        jest.restoreAllMocks();
+    });
+
+    it("displays friend requests when there are requests", async () => {   
+        // Act
+        act(()=>{
+            render(<InitPage />);
+        });
+        // Assert
+        await waitFor(() => {
+            expect(screen.getByText(/John/i)).toBeInTheDocument();
+            expect(screen.getByText(/Alice/i)).toBeInTheDocument();
+        });
+    });
+
+    it("does not display friend requests when there are no requests", async () => {    
+        // Act
+        act(()=>{
+            render(<InitPage />);
+        });
+    
+        // Assert
+        await waitFor(() => {
+            expect(screen.queryByText(/John/i)).not.toBeInTheDocument();
+            expect(screen.queryByText(/Alice/i)).not.toBeInTheDocument();
+        });
+    });
 });
+
+describe("InitPage component about yes or no", () => {
+    beforeAll(() => {
+        window.alert = jest.fn();
+        (useRouter as jest.Mock).mockReturnValue(mockRouter);
+        // 模拟 fetch 请求
+        global.fetch = jest.fn().mockImplementation((url, options) => {
+            if (url === "/api/user/get_friend_requests/") {
+                return Promise.resolve({
+                    json: () => Promise.resolve({ code: 0, requests: mockRequests2 }),
+                });
+            } 
+            else if (url === "/api/user/respond_friend_request/") {
+                return Promise.resolve({
+                    json: () => Promise.resolve({ code: 0 }),
+                });
+            } 
+            else {
+                return Promise.reject(new Error("Invalid URL"));
+            }
+        });
+    });
+
+    afterEach(() => {
+        jest.restoreAllMocks();
+    });
+
+    it("sends respond when accept is clicked", async () => {
+        act(() => {
+            render(<InitPage />);
+        });
+
+        await waitFor(() => {
+            expect(screen.getByText(/John/i)).toBeInTheDocument();
+        });
+
+        const acceptButton = screen.getByText(/同意/i);
+
+        act(() => {
+            fireEvent.click(acceptButton);
+        });
+
+        // 等待 sendRespond 函数完成
+        await waitFor(() => {
+            expect(global.fetch).toHaveBeenCalledTimes(3);
+        });
+
+        // 断言 sendRespond 函数发送的请求参数是否符合预期
+        expect(global.fetch).toHaveBeenCalledWith(
+            "/api/user/respond_friend_request/",
+            {
+                method: "POST",
+                credentials: "include",
+                body: JSON.stringify({
+                    response: "accept",
+                    friend_user_id: 1,
+                }),
+            }
+        );
+    });
+
+    it("sends respond when reject is clicked", async () => {
+        act(() => {
+            render(<InitPage />);
+        });
+
+        await waitFor(() => {
+            expect(screen.getByText(/John/i)).toBeInTheDocument();
+        });
+
+        const rejectButton = screen.getByText(/拒绝/i);
+
+        act(() => {
+            fireEvent.click(rejectButton);
+        });
+
+        // 等待 sendRespond 函数完成
+        await waitFor(() => {
+            expect(global.fetch).toHaveBeenCalledTimes(3);
+        });
+
+        // 断言 sendRespond 函数发送的请求参数是否符合预期
+        expect(global.fetch).toHaveBeenCalledWith(
+            "/api/user/respond_friend_request/",
+            {
+                method: "POST",
+                credentials: "include",
+                body: JSON.stringify({
+                    response: "reject",
+                    friend_user_id: 1,
+                }),
+            }
+        );
+    });
+});
+
+
--- src/tests/info.test.tsx
+++ src/tests/info.test.tsx
@@ -1,5 +1,6 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { useRouter } from "next/router";
+import { act } from "react-dom/test-utils";
 import "regenerator-runtime/runtime";
 import InitPage from "../pages/user/info";
 
@@ -8,40 +9,18 @@ jest.mock("next/router", () => ({
     useRouter: jest.fn(),
 }));
 
-jest.mock('node-fetch', () => {
-    return jest.fn(() =>
-      Promise.resolve({
-        json: () => Promise.resolve({
-          name: "Test User",
-          avatar: "test-avatar.png",
-        }),
-      })
-    );
-  });
-  
-
 describe("InitPage component", () => {
-
+    
     beforeEach(() => {
         (useRouter as jest.Mock).mockReturnValue({ push: jest.fn() });
-        (global as any).fetch = jest.fn(() =>
+        (global as any).fetch = jest.fn(()=>
             Promise.resolve({
                 json: () => Promise.resolve({
-                    name: "Mock User",
-                    avatar: "mock_avatar.png",
+                    name: "Test User",
+                    avatar: "test-avatar.png",
                 }),
             })
-        ) as jest.MockedFunction<typeof fetch>;
-
-        // 模拟数据
-        fetch.mockImplementation(() =>
-          Promise.resolve({
-            json: () => Promise.resolve({
-              name: "Test User",
-              avatar: "test-avatar.png",
-            }),
-          })
-        );
+        );     
     });
 
     afterEach(() => {
@@ -49,59 +28,12 @@ describe("InitPage component", () => {
     });
 
     it("renders the component without errors", () => {
-        render(<InitPage />);
+        act(()=>{
+            render(<InitPage />);
+        });
         expect(screen.getByText(/修改头像/i)).toBeInTheDocument();
         expect(screen.getByText(/修改用户名/i)).toBeInTheDocument();
         expect(screen.getByText(/修改密码/i)).toBeInTheDocument();
         expect(screen.getByText(/注销本用户/i)).toBeInTheDocument();
     });
-
-    // it("allows user to upload avatar file", async () => {
-    //     render(<InitPage />);
-    //     fireEvent.change(screen.getByPlaceholderText(/uploaded image/i), {
-    //         target: { files: [new File(["(⌐□_□)"], "chucknorris.png", { type: "image/png" })] },
-    //     });
-    //     expect(screen.getByDisplayValue(/chucknorris\.png/i)).toBeInTheDocument();
-    //     fireEvent.submit(screen.getByText(/上传头像/i).closest("form") as HTMLElement);
-    //     expect(await screen.findByText(/已提交/i)).toBeInTheDocument();
-    // });
-
-    // it("validates and updates username", async () => {
-    //     render(<InitPage />);
-    //     fireEvent.click(screen.getByText(/修改用户名/i));
-    //     fireEvent.change(screen.getByPlaceholderText(/请输入新的用户名/i), { target: { value: "invalid name" } });
-    //     expect(screen.getByText(/\*用户名必须由3-16位字母、数字和下划线组成/i)).toBeInTheDocument();
-    //     expect(screen.getByRole("button", { name: /保存/i })).toBeDisabled();
-
-    //     fireEvent.change(screen.getByPlaceholderText(/请输入新的用户名/i), { target: { value: "newusername" } });
-    //     expect(screen.queryByText(/\*用户名必须由3-16位字母、数字和下划线组成/i)).not.toBeInTheDocument();
-    //     expect(screen.getByRole("button", { name: /保存/i })).not.toBeDisabled();
-
-    //     fireEvent.submit(screen.getByText(/保存/i).closest("form") as HTMLElement);
-    //     expect(await screen.findByText(/成功修改用户名为newusername/i)).toBeInTheDocument();
-    // });
-
-    // it("validates and updates password", async () => {
-    //     render(<InitPage />);
-    //     fireEvent.click(screen.getByText(/修改密码/i));
-    //     fireEvent.change(screen.getByPlaceholderText(/请输入原密码/i), { target: { value: "wrongpassword" } });
-    //     fireEvent.change(screen.getByPlaceholderText(/请输入新的密码/i), { target: { value: "invalid" } });
-    //     expect(screen.getByText(/\*密码必须由6-16位字母、数字和下划线组成/i)).toBeInTheDocument();
-    //     expect(screen.getByRole("button", { name: /保存/i })).toBeDisabled();
-
-    //     fireEvent.change(screen.getByPlaceholderText(/请输入新的密码/i), { target: { value: "new_password123" } });
-    //     expect(screen.queryByText(/\*密码必须由6-16位字母、数字和下划线组成/i)).not.toBeInTheDocument();
-    //     expect(screen.getByRole("button", { name: /保存/i })).not.toBeDisabled();
-
-    //     fireEvent.submit(screen.getByText(/保存/i).closest("form") as HTMLElement);
-    //     expect(await screen.findByText(/成功修改密码/i)).toBeInTheDocument();
-    // });
-
-    // it("allows user to delete account", async () => {
-    //     render(<InitPage />);
-    //     jest.spyOn(window, "confirm").mockImplementation(() => true);
-    //     fireEvent.click(screen.getByText(/注销本用户/i));
-    //     expect(await screen.findByText(/注销成功/i)).toBeInTheDocument();
-    // });
 });
-
