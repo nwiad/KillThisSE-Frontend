52098feeb220b2ca76c5de807902acb4f4efc925
刘蕴皓 <jonhannliu@JonhannLius-MacBook-Pro.local>
2023-04-19 22:00:45 +0800

    fix:增加了导航栏显示当前用户

--- src/pages/user/navbar.tsx
+++ src/pages/user/navbar.tsx
@@ -1,9 +1,11 @@
 import { useRouter } from "next/router";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { websocket, createWebSocket, closeWebSocket } from "../../utils/websocket";
 
 const Navbar = () => {
     const [msg, setMsg] = useState<string>("");
+    const [name, setName] = useState<string>("");
+    const [avatar, setAvatar] = useState<string>();
     
     const router = useRouter();
 
@@ -40,6 +42,26 @@ const Navbar = () => {
             .catch((err) => alert(err));
     };
 
+    useEffect(() => {
+        fetch(
+            "/api/user/get_profile/",
+            {
+                method: "POST",
+                credentials: "include",
+                body: JSON.stringify({
+                    token: localStorage.getItem("token")
+                })
+            }
+        )
+            .then((res) => res.json())
+            .then((data) => {
+                setName(data.name);
+                setAvatar(data.avatar);
+    
+            })
+            .catch((err) => alert(err));
+    })
+
     return (
         <nav style={{padding: 12}}>
             <ul className="navbar">
@@ -49,10 +71,9 @@ const Navbar = () => {
                 <li className="navbar_ele_r" onClick={() => {router.push("/user/friendindex");}}>
                         好友
                 </li>
-                <input className="msg_box" type="text" value={msg} onChange={(e) => setMsg(e.target.value)} />
-                <button onClick={() => sendMsg()}>send</button>
-                <li className="navbar_ele_l" onClick={() => {router.push("/user/info");}}>
-                        个人中心
+                <li className="navbar_ele_info" onClick={() => {router.push("/user/info");}}>
+                        <p>{name}</p>
+                        <img className="navbarAvatar" src={`${avatar}`}/>
                 </li>
                 <li className="navbar_ele_l" onClick={() => {userLogout(); router.push("/");}}>
                         登出
